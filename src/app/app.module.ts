59013b5d847d2c485650cb873b416a23f5d5b4aa
Dave <dave@webdave.de>
2019-09-11 13:47:56 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyNavComponent } from './shared/my-nav/my-nav.component';
+import { BooksModule } from './features/books/books.module';
+import { AboutModule } from './features/about/about.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MyNavComponent],
+  imports: [BrowserModule, AppRoutingModule, BooksModule, AboutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
