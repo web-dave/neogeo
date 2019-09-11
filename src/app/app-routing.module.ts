31447fb0659daa328800b09ffc12e11bb35ac19b
Dave <dave@webdave.de>
2019-09-11 15:06:49 +0200

    child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BooksComponent } from './features/books/books.component';
-import { AboutComponent } from './features/about/about.component';
 
 const routes: Routes = [
   {
-    path: 'books',
-    component: BooksComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
     path: '',
     redirectTo: '/books',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: '/about',
     pathMatch: 'full'
   }
 ];
--- src/app/features/books/books.module.ts
+++ src/app/features/books/books.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [BooksComponent],
-  exports: [BooksComponent],
+  declarations: [BooksComponent, BookListComponent],
   imports: [CommonModule, BooksRoutingModule]
 })
 export class BooksModule {}
